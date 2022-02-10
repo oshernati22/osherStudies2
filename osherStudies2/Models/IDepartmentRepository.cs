48549729ba74d461981789c06464a26a3d142423
Osher <oshernati22@gmail.com>
2022-02-10 13:24:04 +0200

    Add project files.

---
+++ osherStudies2/Models/IDepartmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using osherStudies2Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace osherStudies2.Models
+{
+    public interface IDepartmentRepository
+    {
+        Task<IEnumerable<Department>> GetDepartments();
+        Task<Department> GetDepartment(int departmentId);
+    }
+}
